dc899b693125802b4b8d7ca894eb371b86448b75
Ehtezaz Ahmed <Ehtezaz Ahmed@172.16.105.107>
2016-05-30 17:46:37 +0500

    Message 5

--- src/ehtezazahmed/TestClass2.java
+++ src/ehtezazahmed/TestClass2.java
@@ -5,4 +5,8 @@ public class TestClass2 {
 	public TestClass2() {
 		// TODO Auto-generated constructor stub
 	}
+	
+	public void Show(){
+		System.out.print("TEST REPOSITORY..   Reverse...");
+	}
 }
